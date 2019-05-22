f2ceaa7b2ebc0d8b967514ddde601969c7942a68
stanislawK <stanislaw.krupienko@gmail.com>
2019-05-22 21:33:42 +0200

    indentations and dockstring fixes

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -39,7 +39,7 @@ def client(app):
 
 @pytest.fixture
 def _db():
-    """Create and configure a new db instance for pytest-flask-sqlalchemy"""
+    """Create and configure a new db instance for pytest-flask-sqlalchemy."""
     db_fd, db_path = tempfile.mkstemp()
     app = create_app()
     app.config['TESTING'] = True
@@ -59,15 +59,19 @@ def _db():
 
 @pytest.fixture
 def new_user():
-    user = {'username': 'TestName',
-            'password': 'TestPassword'}
+    user = {
+        'username': 'TestName',
+        'password': 'TestPassword'
+    }
     return user
 
 
 @pytest.fixture
 def registered_user(new_user, app, _db):
-    new_user = User(username=new_user['username'],
-                    password=new_user['password'])
+    new_user = User(
+        username=new_user['username'],
+        password=new_user['password']
+    )
     with app.app_context():
         db = _db
         db.session.add(new_user)
@@ -77,16 +81,21 @@ def registered_user(new_user, app, _db):
 
 @pytest.fixture
 def access_token(client, new_user, registered_user):
-    rv = client.post('/auth/login', json={'username': new_user['username'],
-                                          'password': new_user['password']})
+    rv = client.post(
+        '/auth/login',
+        json={'username': new_user['username'],
+              'password': new_user['password']}
+    )
     access_token = rv.get_json()['access_token']
     return access_token
 
 
 @pytest.fixture
 def new_msg():
-    msg = {"name": "TestName",
-           "email": "test@test.com",
-           "phone": "777222333",
-           "content": "Lorem Ipsum cos tam cos tam"}
+    msg = {
+        "name": "TestName",
+        "email": "test@test.com",
+        "phone": "777222333",
+        "content": "Lorem Ipsum cos tam cos tam"
+    }
     return msg
--- backend/tests/test_contact_us.py
+++ backend/tests/test_contact_us.py
@@ -4,11 +4,7 @@ from backend.extensions import mail
 
 
 def test_contact_us_endpoint(client, new_msg):
-    """
-    GIVEN an application instance, valid message, and mocked email outbox
-    WHEN sending valid contact message to send-email endpoint
-    THEN check status code, message, and email outbox
-    """
+    """Test sending valid message to send-email enndoint."""
     with mail.record_messages() as outbox:
         rv = client.post('/send-email/', json=new_msg)
         response = rv.get_json()
@@ -21,11 +17,7 @@ def test_contact_us_endpoint(client, new_msg):
 
 
 def test_contact_us_without_email(client, new_msg):
-    """
-    GIVEN an application instance and message without email
-    WHEN sending invalid contact message to send-email endpoint
-    THEN check if proper error was raised with 400 status code
-    """
+    """Test sending message with no email provided."""
     del new_msg["email"]
     rv = client.post('/send-email/', json=new_msg)
     response = rv.get_json()
@@ -35,11 +27,7 @@ def test_contact_us_without_email(client, new_msg):
 
 
 def test_contact_us_without_content(client, new_msg):
-    """
-    GIVEN an application instance and message without content
-    WHEN sending invalid contact message to send-email endpoint
-    THEN check if proper error was raised with 400 status code
-    """
+    """Test sending message with no content provided."""
     del new_msg["content"]
     rv = client.post('/send-email/', json=new_msg)
     response = rv.get_json()
