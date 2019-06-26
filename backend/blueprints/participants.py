6605bdfcf678aba0c4d1ddb04e4808192cfa4bde
stanislawK <stanislaw.krupienko@gmail.com>
2019-06-26 20:47:26 +0200

    Added get participant endpoint

--- backend/blueprints/participants.py
+++ backend/blueprints/participants.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
 from backend.models import Participant
-from backend.serializers.participant_serializer import participants_schema
+from backend.serializers.participant_serializer import (
+    participant_schema, participants_schema
+)
 
 participants = Blueprint('participants', __name__)
+
+
+@participants.route('/participant/<int:id>', methods=['GET'])
+@jwt_required
+def get_participant(id):
+    participant = Participant.find_by_id(id)
+
+    if participant:
+        return jsonify(participant_schema.dump(participant)), 200
+    return jsonify({"message": "Participant not found"}), 404
 
 
 @participants.route('/participants', methods=['GET'])
--- backend/serializers/participant_serializer.py
+++ backend/serializers/participant_serializer.py
@@ -3,14 +3,36 @@ from marshmallow import Schema, fields, validate
 
 class ParticipantSchema(Schema):
     class Meta:
-        fields = ('name', 'lastname', 'email', 'github', 'phone')
+        fields = (
+            'id', 'name', 'lastname', 'email', 'github', 'hacknights', 'phone'
+        )
+        dump_only = ('id', 'hacknights')
 
     name = fields.Str(required=True, validate=[validate.Length(max=50)])
     lastname = fields.Str(validate=[validate.Length(max=50)])
     email = fields.Email(validate=[validate.Length(max=200)])
     github = fields.Str(validate=[validate.Length(max=200)])
+    hacknights = fields.Nested(
+        'HacknightSchema',
+        exclude=('participants', ),
+        many=True
+    )
     phone = fields.Str(validate=[validate.Length(max=13)])
+
+
+class HacknightSchema(Schema):
+    class Meta:
+        fields = ('id', 'date', 'participants')
+        dump_only = ('id',)
+
+    participants = fields.Nested(
+        ParticipantSchema,
+        only=('id', 'github'),
+        many=True
+    )
 
 
 participant_schema = ParticipantSchema()
 participants_schema = ParticipantSchema(many=True)
+hacknight_schema = HacknightSchema()
+hacknights_schema = HacknightSchema(many=True)
