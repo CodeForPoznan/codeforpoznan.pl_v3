77347cfdb54b1e70d67d361bf189d204e91f8651
w1stler <wojciech.patelka@gmail.com>
2019-07-17 20:53:24 +0200

    [59] Create participant endpoint

--- backend/blueprints/participants.py
+++ backend/blueprints/participants.py
@@ -1,7 +1,11 @@
+from backend.extensions import db
+
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
 from backend.models import Participant
-from backend.serializers.participant_serializer import participants_schema
+from backend.serializers.participant_serializer import ParticipantSchema
+
+from marshmallow import ValidationError
 
 participants = Blueprint('participants', __name__)
 
@@ -11,6 +15,36 @@ participants = Blueprint('participants', __name__)
 def get_participants_list():
     participants_list = Participant.query.all()
     if participants_list:
-        participants = participants_schema.dump(participants_list)
+        participants = ParticipantSchema.dump(participants_list)
         return jsonify({"participants": participants}), 200
     return jsonify({"message": "participants not found"}), 404
+
+
+@participants.route('/participants', methods=['POST'])
+@jwt_required
+def create_praticipant():
+    json_data = request.get_json(force=True)
+    if not json_data:
+        return {'message': 'No input data provided'}, 400
+
+    schema = ParticipantSchema()
+    try:
+        data = schema.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    participant = Participant.query.filter_by(name=data['name']).first()
+    if participant:
+        return jsonify({"message": "Participant already exists."}), 400
+    participant = Participant(
+        name=json_data['name'],
+        lastname=json_data['lastname'],
+        email=json_data['email'],
+        github=json_data['github'],
+        phone=json_data['phone']
+    )
+    db.session.add(participant)
+    db.session.commit()
+    # result = ParticipantSchema.dump(participant).data
+
+    return jsonify({"message": "Participant created successfully."}), 201
