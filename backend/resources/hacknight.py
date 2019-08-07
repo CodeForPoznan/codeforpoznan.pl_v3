21395c995a43a3ab82cfaee7cf7cfbdca94e8c5a
stanislawK <stanislaw.krupienko@gmail.com>
2019-08-07 20:58:44 +0200

    changed way how schemas are imported

--- backend/resources/hacknight.py
+++ backend/resources/hacknight.py
@@ -4,11 +4,13 @@ from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
 from backend.models import Hacknight
-from backend.serializers.hacknight_serializer import hacknights_schema
+from backend.serializers.hacknight_serializer import HacknightSchema
 
 
 class HacknightList(Resource):
     @jwt_required
     def get(self):
-        hacknights = hacknights_schema.dump(Hacknight.query.all())
-        return {'hacknights': hacknights}, HTTPStatus.OK
+        hacknight_schema = HacknightSchema(
+            many=True, exclude=('participants',))
+        return {'hacknights': hacknight_schema.dump(
+            Hacknight.query.all())}, HTTPStatus.OK
