901bc0bd42f450fd5816a94cd9624644a1f5fe40
stanislawK <stanislaw.krupienko@gmail.com>
2019-09-15 15:14:24 +0200

    Vuetify upgraded with all dependencies

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuelidate from 'vuelidate';
-import Vuetify from 'vuetify';
+import Vuetify from 'vuetify/lib';
 import 'vuetify/dist/vuetify.min.css';
 
 import router from './router';
 import store from './store';
+import vuetify from './plugins/vuetify';
 
 Vue.use(Vuelidate);
 Vue.use(Vuetify);
@@ -15,5 +16,6 @@ Vue.config.productionTip = false;
 new Vue({
     router,
     store,
+    vuetify,
     render: h => h(App)
 }).$mount('#app');
