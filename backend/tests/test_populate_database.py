68ce2a290304e1a7fb0d1f409033ed762327efe5
stanislawK <stanislaw.krupienko@gmail.com>
2019-11-01 20:00:37 +0100

    Fixed test_populate_database

--- backend/tests/test_populate_database.py
+++ backend/tests/test_populate_database.py
@@ -8,19 +8,21 @@ from backend.models import Hacknight, Participant, User
 
 @pytest.fixture
 def script(app):
-    return ScriptInfo(create_app=app)
+    return ScriptInfo(create_app=lambda info: app)
 
 
-def test_populate_database(app, _db, script):
+def test_populate_database(script):
     """Test populate_database command."""
     runner = CliRunner()
-    result = runner.invoke(populate_database, obj=app)
+    result = runner.invoke(populate_database, obj=script)
 
+    app = script._loaded_app
     assert result.exit_code == 0
     assert "Creating 30 hacknights" in result.output
-    assert len(Hacknight.query.all()) == 30
     assert "Creating 50 participants" in result.output
-    assert len(Participant.query.all()) == 50
     assert "Creating 5 users" in result.output
-    assert len(User.query.all()) == 5
     assert "Created users:" in result.output
+    with app.app_context():
+        assert len(Hacknight.query.all()) == 30
+        assert len(Participant.query.all()) == 50
+        assert len(User.query.all()) == 5
