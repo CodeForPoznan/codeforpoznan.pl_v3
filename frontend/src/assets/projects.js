ebff8267d56e180b7fe0f3f3074763dfa604b89a
OtisRed <durczak.krzysztof@gmail.com>
2019-11-13 21:16:59 +0100

    working js file

---
+++ frontend/src/assets/projects.js
@@ -0,0 +1,56 @@
+const projects = [
+    {
+        description:
+            'Portal Volontulo powstał dla ludzi i organizacji skupionych wokół idei pomocy innym poprzez udział we wolontariacie. Celem projektu jest pomoc we wzajemnym odnalezieniu się ludzi, którzy chcą realizować się jako wolontariusze/szki oraz organizacji i instytucji, które takich osób poszukują. Podział na strefę "Wolontariusza" oraz "Strefę organizacji i instytucji" umożliwa użytkownikom zwinną nawigację na stronie.',
+        github: 'https://github.com/CodeForPoznan/volontulo',
+        image: require('@/assets/images/volontulo.png'),
+        license: 'MIT',
+        licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
+        name: 'Volontulo',
+        partner: 'Wielkopolska Rada Koordynacyjna',
+        partnerPage: 'https://centrum.wrk.org.pl/',
+        stack: [
+            {
+                type: 'frontend',
+                name: 'Angular',
+                version: '2.0',
+                documentation: 'https://angular.io/'
+            },
+            {
+                type: 'backend',
+                name: 'Django',
+                version: '2.2',
+                documentation: 'https://docs.djangoproject.com/en/2.2/'
+            }
+        ],
+        website: 'http://volontuloapp.org/o'
+    },
+    {
+        description:
+            'Portal Volontulo powstał dla ludzi i organizacji skupionych wokół idei pomocy innym poprzez udział we wolontariacie. Celem projektu jest pomoc we wzajemnym odnalezieniu się ludzi, którzy chcą realizować się jako wolontariusze/szki oraz organizacji i instytucji, które takich osób poszukują. Podział na strefę "Wolontariusza" oraz "Strefę organizacji i instytucji" umożliwa użytkownikom zwinną nawigację na stronie.',
+        github: 'https://github.com/CodeForPoznan/volontulo',
+        image: require('@/assets/images/volontulo.png'),
+        license: 'MIT',
+        licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
+        name: 'Volontulo',
+        partner: 'Wielkopolska Rada Koordynacyjna',
+        partnerPage: 'https://centrum.wrk.org.pl/',
+        stack: [
+            {
+                type: 'frontend',
+                name: 'Angular',
+                version: '2.0',
+                documentation: 'https://angular.io/'
+            },
+            {
+                type: 'backend',
+                name: 'Django',
+                version: '2.2',
+                documentation: 'https://docs.djangoproject.com/en/2.2/'
+            }
+        ],
+        website: 'http://volontuloapp.org/o'
+    }
+];
+
+export default projects;
--- frontend/src/components/OurProjects.vue
+++ frontend/src/components/OurProjects.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-container text-xs-center>
+    <v-container class="white">
         <v-layout row id="black-title">
             <v-flex>
                 <v-card flat color="transparent">
@@ -50,6 +50,7 @@
 
 <script>
 import ModalContent from './ModalContent.vue';
+import projects_db from '../assets/projects';
 export default {
     components: {
         'app-modal-content': ModalContent
@@ -58,135 +59,136 @@ export default {
         return {
             dialog: false,
             hoveredImg: require('@/assets/images/magnifier.svg'),
-            projects: [
-                {
-                    description: '',
-                    github: '',
-                    image: '',
-                    license: '',
-                    licensePage: '',
-                    name: 'Fleet manager',
-                    partner: 'Polska Akcja Humanitarna',
-                    partnerPage: 'https://www.pah.org.pl/',
-                    stack: [
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        },
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        }
-                    ],
-                    website: ''
-                },
-                {
-                    description: '',
-                    github: '',
-                    image: '',
-                    license: '',
-                    licensePage: '',
-                    name: 'Alinka',
-                    partner: '',
-                    partnerPage: '',
-                    stack: [
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        },
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        }
-                    ],
-                    website: ''
-                },
-                {
-                    description:
-                        'Portal Volontulo powstał dla ludzi i organizacji skupionych wokół idei pomocy innym poprzez udział we wolontariacie. Celem projektu jest pomoc we wzajemnym odnalezieniu się ludzi, którzy chcą realizować się jako wolontariusze/szki oraz organizacji i instytucji, które takich osób poszukują. Podział na strefę "Wolontariusza" oraz "Strefę organizacji i instytucji" umożliwa użytkownikom zwinną nawigację na stronie.',
-                    github: 'https://github.com/CodeForPoznan/volontulo',
-                    image: require('@/assets/images/volontulo.png'),
-                    license: 'MIT',
-                    licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
-                    name: 'Volontulo',
-                    partner: 'Wielkopolska Rada Koordynacyjna',
-                    partnerPage: 'https://centrum.wrk.org.pl/',
-                    stack: [
-                        {
-                            type: 'frontend',
-                            name: 'Angular',
-                            version: '2.0',
-                            documentation: 'https://angular.io/'
-                        },
-                        {
-                            type: 'backend',
-                            name: 'Django',
-                            version: '2.2',
-                            documentation:
-                                'https://docs.djangoproject.com/en/2.2/'
-                        }
-                    ],
-                    website: 'http://volontuloapp.org/o'
-                },
-                {
-                    description: '',
-                    github: '',
-                    image: require('@/assets/images/bank_empatii.png'),
-                    license: '',
-                    licensePage: '',
-                    name: 'Bank empatii',
-                    partner: '',
-                    partnerPage: '',
-                    stack: [
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        },
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        }
-                    ],
-                    website: ''
-                },
-                {
-                    description: '',
-                    github: '',
-                    image: require('@/assets/images/wysadz_ulice.png'),
-                    license: '',
-                    licensePage: '',
-                    name: 'Wysadź ulicę',
-                    partner: '',
-                    partnerPage: '',
-                    stack: [
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        },
-                        {
-                            type: '',
-                            name: '',
-                            version: '',
-                            documentation: ''
-                        }
-                    ],
-                    website: ''
-                }
-            ],
+            projects: projects_db,
+            // projects: [
+            //     {
+            //         description: '',
+            //         github: '',
+            //         image: '',
+            //         license: '',
+            //         licensePage: '',
+            //         name: 'Fleet manager',
+            //         partner: 'Polska Akcja Humanitarna',
+            //         partnerPage: 'https://www.pah.org.pl/',
+            //         stack: [
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             },
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             }
+            //         ],
+            //         website: ''
+            //     },
+            //     {
+            //         description: '',
+            //         github: '',
+            //         image: '',
+            //         license: '',
+            //         licensePage: '',
+            //         name: 'Alinka',
+            //         partner: '',
+            //         partnerPage: '',
+            //         stack: [
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             },
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             }
+            //         ],
+            //         website: ''
+            //     },
+            //     {
+            //         description:
+            //                 'Portal Volontulo powstał dla ludzi i organizacji skupionych wokół idei pomocy innym poprzez udział we wolontariacie. Celem projektu jest pomoc we wzajemnym odnalezieniu się ludzi, którzy chcą realizować się jako wolontariusze/szki oraz organizacji i instytucji, które takich osób poszukują. Podział na strefę "Wolontariusza" oraz "Strefę organizacji i instytucji" umożliwa użytkownikom zwinną nawigację na stronie.',
+            //         github: 'https://github.com/CodeForPoznan/volontulo',
+            //         image: require('@/assets/images/volontulo.png'),
+            //         license: 'MIT',
+            //         licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
+            //         name: 'Volontulo',
+            //         partner: 'Wielkopolska Rada Koordynacyjna',
+            //         partnerPage: 'https://centrum.wrk.org.pl/',
+            //         stack: [
+            //             {
+            //                 type: 'frontend',
+            //                 name: 'Angular',
+            //                 version: '2.0',
+            //                 documentation: 'https://angular.io/'
+            //             },
+            //             {
+            //                 type: 'backend',
+            //                 name: 'Django',
+            //                 version: '2.2',
+            //                 documentation:
+            //                         'https://docs.djangoproject.com/en/2.2/'
+            //             }
+            //         ],
+            //         website: 'http://volontuloapp.org/o'
+            //     },
+            //     {
+            //         description: '',
+            //         github: '',
+            //         image: require('@/assets/images/bank_empatii.png'),
+            //         license: '',
+            //         licensePage: '',
+            //         name: 'Bank empatii',
+            //         partner: '',
+            //         partnerPage: '',
+            //         stack: [
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             },
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             }
+            //         ],
+            //         website: ''
+            //     },
+            //     {
+            //         description: '',
+            //         github: '',
+            //         image: require('@/assets/images/wysadz_ulice.png'),
+            //         license: '',
+            //         licensePage: '',
+            //         name: 'Wysadź ulicę',
+            //         partner: '',
+            //         partnerPage: '',
+            //         stack: [
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             },
+            //             {
+            //                 type: '',
+            //                 name: '',
+            //                 version: '',
+            //                 documentation: ''
+            //             }
+            //         ],
+            //         website: ''
+            //     }
+            // ],
             selectedProject: []
         };
     },
