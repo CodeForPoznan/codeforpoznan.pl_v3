5b6bc3486eef9440e59c2d636ffb9243d02f0fb7
Wojciech Patelka <wojciech.patelka@gmail.com>
2019-11-29 12:59:35 +0100

    [74] implement black (fixes #68 #74) (#187)
    Co-Authored-By: Artur Tamborski <tamborskiartur@gmail.com>

--- backend/serializers/participant_serializer.py
+++ backend/serializers/participant_serializer.py
@@ -3,17 +3,21 @@ from marshmallow import Schema, fields, validate
 
 class ParticipantSchema(Schema):
     class Meta:
-        fields = ('id', 'first_name', 'last_name', 'email', 'github', 'hacknights', 'phone')
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "github",
+            "hacknights",
+            "phone",
+        )
 
-        dump_only = ('id', 'hacknights')
+        dump_only = ("id", "hacknights")
 
     first_name = fields.Str(required=True, validate=[validate.Length(max=50)])
     last_name = fields.Str(validate=[validate.Length(max=50)])
     email = fields.Email(validate=[validate.Length(max=200)])
     github = fields.Str(validate=[validate.Length(max=200)])
-    hacknights = fields.Nested(
-        'HacknightSchema',
-        exclude=('participants', ),
-        many=True
-    )
+    hacknights = fields.Nested("HacknightSchema", exclude=("participants",), many=True)
     phone = fields.Str(validate=[validate.Length(max=13)])
