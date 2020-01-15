32bbe25b0d988425ea1dea1454f7d96c965d7dce
Krystyna <krystyna.m.piatkowska@gmail.com>
2020-01-15 20:55:41 +0100

    participant put test with valid data

--- backend/tests/test_participant_details.py
+++ backend/tests/test_participant_details.py
@@ -22,12 +22,31 @@ def test_get_delete_participant_when_logged_in(
     else:
         assert response["message"] == "Participant deleted successfully."
 
+@pytest.mark.parametrize("method", ["put"])
+def test_put_participant_when_logged_in(
+    app, client, access_token, add_participants, method
+):
+    """Test put participant details for logged in user and valid data."""
+    with app.app_context():
+        payload={"last_name": "TestTest", "email": "testtest@test.pl"}
+        rv = client.put(
+            "/participants/1/",
+            headers={"Authorization": "Bearer {}".format(access_token)},
+            json=payload,
+        )
+        response = rv.get_json()
+        schema = ParticipantSchema()
+        participant = schema.dump(Participant.query.first())
+    assert rv.status_code == HTTPStatus.OK
+    assert response["last_name"]==payload["last_name"]
+    assert response["email"]==payload["email"]
+    assert participant["last_name"]==payload["last_name"]
 
 @pytest.mark.parametrize("method", ["get", "delete", "put"])
 def test_get_delete_put_non_existing_participant(
     client, access_token, add_participants, method
 ):
-    """Test get and delete non-existing participant."""
+    """Test get, put and delete non-existing participant."""
     rv = getattr(client, method)(
         "/participants/11/", headers={"Authorization": "Bearer {}".format(access_token)}
     )
@@ -36,7 +55,7 @@ def test_get_delete_put_non_existing_participant(
 
 @pytest.mark.parametrize("method", ["get", "delete", "put"])
 def test_get_delete_put_participant_unauthorized(client, add_participants, method):
-    """Test get and delete participant with not logged in user."""
+    """Test get, put and delete participant with not logged in user."""
     rv = client.get("/participants/1/")
     response = rv.get_json()
     assert rv.status_code == HTTPStatus.UNAUTHORIZED
