8222dde508502b75ad396ec9bbe1881c48538f79
OtisRed <durczak.krzysztof@gmail.com>
2020-02-05 21:52:05 +0100

    recent version

--- frontend/src/components/ModalContent.vue
+++ frontend/src/components/ModalContent.vue
@@ -19,47 +19,85 @@
         </v-card-actions>
       </v-row>
     </v-card-title>
-    <v-row class="ma-4">
-      <v-btn text rounded :href="selectedProject.licensePage" target="_blank">
-        <v-icon>far fa-copyright</v-icon>
-        <p class="buttons">Licencja {{ selectedProject.licenseName }}</p>
-      </v-btn>
-      <v-btn text rounded :href="selectedProject.websiteLink" target="_blank">
-        <v-icon>language</v-icon>
-        <p class="buttons">Strona WWW</p>
-      </v-btn>
-      <v-btn text rounded :href="selectedProject.githubLink" target="_blank">
-        <v-icon>fab fa-github</v-icon>
-        <p class="buttons">Repozytorium</p>
-      </v-btn>
-    </v-row>
-    <v-row class="ma-6">
-      <p class="modal-subtitle">
-        Partner projektu:
-      </p>
-      <v-btn text rounded :href="selectedProject.partnerPage" target="_blank">
-        <p class="modal-subtitle">{{ selectedProject.partner }}</p>
-      </v-btn>
-    </v-row>
-    <v-row>
-      <p class="content-black">{{ selectedProject.description }}</p>
-    </v-row>
-    <v-row class="ml-3 mr-3">
-      <p class="modal-subtitle">Wykorzystane technologie:</p>
-    </v-row>
-    <v-row class="mr-1 ml-1">
-      <v-btn
-        v-for="(item, index) in selectedProject.stack"
-        :key="index"
-        :href="item.documentation"
-        class="stack-list"
-        target="_blank"
-        text
-        rounded
-      >
-        {{ item.type }}: {{ item.name }} {{ item.version }}
-      </v-btn>
-    </v-row>
+    <v-card-actions>
+      <v-row>
+        <v-btn
+          class="buttons"
+          text
+          rounded
+          large
+          :href="selectedProject.licensePage"
+          target="_blank"
+        >
+          <v-icon>far fa-copyright</v-icon>
+          <p class="buttons--text">
+            Licencja {{ selectedProject.licenseName }}
+          </p>
+        </v-btn>
+        <v-btn
+          class="buttons"
+          text
+          rounded
+          large
+          :href="selectedProject.githubLink"
+          target="_blank"
+        >
+          <v-icon>fab fa-github</v-icon>
+          <p class="buttons--text">Repozytorium</p>
+        </v-btn>
+        <v-btn
+          class="buttons"
+          text
+          rounded
+          large
+          :href="selectedProject.websiteLink"
+          target="_blank"
+        >
+          <v-icon>language</v-icon>
+          <p class="buttons--text">Strona projektu</p>
+        </v-btn>
+        <v-btn
+          class="buttons"
+          text
+          rounded
+          large
+          :href="selectedProject.partnerPage"
+          target="_blank"
+        >
+          <v-icon>fas fa-handshake</v-icon>
+          <p class="buttons--text">Strona partnera</p>
+        </v-btn>
+      </v-row>
+    </v-card-actions>
+    <v-card-text>
+      <v-row>
+        <div>
+          <p class="modal-subtitle">Partner projektu:</p>
+          <p class="modal-subtitle">{{ selectedProject.partner }}</p>
+        </div>
+      </v-row>
+      <v-row>
+        <p class="content-black">{{ selectedProject.description }}</p>
+      </v-row>
+      <v-row>
+        <p class="modal-subtitle">Wykorzystane technologie:</p>
+      </v-row>
+    </v-card-text>
+    <v-card-actions>
+      <v-row class="mr-1 ml-1">
+        <v-btn
+          v-for="(item, index) in selectedProject.stack"
+          :key="index"
+          :href="item.documentation"
+          class="stack-list"
+          target="_blank"
+          text
+          rounded
+        >
+          {{ item.type }}: {{ item.name }} {{ item.version }}
+        </v-btn>
+      </v-row>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -82,9 +120,19 @@ a {
 }
 
 .buttons {
+  margin: 0rem 1rem 1rem 2rem;
+}
+
+.buttons--text {
   display: flex;
   align-content: center;
   margin: 0.5rem;
+}
+
+.button-row {
+  display: flex;
+  padding: 8px 36px;
+  align-items: flex-start;
 }
 
 .content-black {
@@ -104,21 +152,26 @@ a {
 }
 
 .modal-subtitle {
-  align-content: center;
+  align-self: auto;
+  justify-content: space-around;
   color: black;
   display: flex;
   font-family: $font-content;
   font-size: 1.5rem;
   font-weight: bold;
   line-height: 1.43;
-  padding: 0;
   position: relative;
+  padding-left: 2rem;
 }
 
 .stack-list {
   background-color: $blue;
   color: $white;
   margin: 4px;
+}
+
+.text-row {
+  padding: 1px;
 }
 
 .title-header {
