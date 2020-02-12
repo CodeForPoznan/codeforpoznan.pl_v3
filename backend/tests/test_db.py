fe22490b191b207dba76fa7981efa2e2b018b333
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2020-02-12 19:09:29 +0100

    Added remove_expired_tokens method (fixes #228) (#229)
    * Added remove_expired_tokens method.
    
    * Tokens variable from remove_expired classmethod removed

--- backend/tests/test_db.py
+++ backend/tests/test_db.py
@@ -1,6 +1,9 @@
-import datetime
+from datetime import date, timedelta
 
-from backend.models import User, Participant, Hacknight
+from flask_jwt_extended import create_access_token
+
+from backend.models import Hacknight, JWTToken, Participant, User
+from backend.resources.auth import add_token_to_database
 
 
 def test_add_new_user_to_db(_db, new_user):
@@ -45,4 +48,20 @@ def test_add_new_hacknight_to_db(_db, new_hacknight):
 
     hacknight = db.session.query(Hacknight).first()
 
-    assert hacknight.date == datetime.date(2000, 10, 10)
+    assert hacknight.date == date(2000, 10, 10)
+
+
+def test_remove_expired_token_method(_db):
+    """Test remove_expired method for valid and expired tokens."""
+    valid_token = create_access_token(identity=1)
+    expired_token = create_access_token(
+        identity=1, expires_delta=timedelta(microseconds=1)
+    )
+    add_token_to_database(valid_token)
+    add_token_to_database(expired_token)
+    tokens_db = _db.session.query(JWTToken).all()
+    expired = [token.expired() for token in tokens_db]
+    assert True in expired and False in expired
+
+    JWTToken.remove_expired()
+    assert len(_db.session.query(JWTToken).all()) == 1
