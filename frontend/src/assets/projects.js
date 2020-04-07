8398ee48a77bab19122443aee35a3230065db49a
MargoWM <38291838+MargoWM@users.noreply.github.com>
2020-04-07 20:11:31 +0200

    Added descriptions to projects

--- frontend/src/assets/projects.js
+++ frontend/src/assets/projects.js
@@ -26,24 +26,24 @@ const projects = [
     websiteLink: 'http://volontuloapp.org/o'
   },
   {
-    description: '',
-    githubLink: '',
+    description: 'W wyniku współpracy z Polską Akcją Humanitarną stworzyliśmy aplikację pomagającą w logistycznych procesach akcji humanitarnych w różnych częściach globu. Aktualnie aplikacja służąca rejestracji i ewidencji przejazdów jest wykorzystywana podczas akcji humanitarnych na Ukrainie, w Sudanie Południowym, Jemenie oraz Somalii.',
+    githubLink: 'https://github.com/CodeForPoznan/pah-fm',
     image: '',
     license: '',
     licensePage: '',
     name: 'Fleet Manager',
-    partner: '',
-    partnerPage: '',
+    partner: 'PAH - Polska Akcja Humanitarna',
+    partnerPage: 'https://www.pah.org.pl/',
     stack: [
       {
         type: 'Frontend',
-        name: '',
+        name: 'JavaScript',
         version: '',
         documentation: ''
       },
       {
         type: 'Backend',
-        name: '',
+        name: 'Python',
         version: '',
         documentation: ''
       }
@@ -51,36 +51,36 @@ const projects = [
     website: ''
   },
   {
-    description: '',
-    githubLink: '',
+    description: ' Aplikacja Alinka powstała w celu optymalizacji codziennej pracy w poradniach psychologiczno-pedagogicznych. Jako byli pracownicy takiej poradni doskonale rozumiemy jakiego nakładu pracy wymaga wydanie orzeczenia czy sporządzenie protokołu z posiedzenia komisji. Aplikacja Alinka jest odpowiedzią na trudności z jakimi spotykają się w codziennej pracy pracownicy administracji poradni. Nazwa aplikacji: „Alinka” to imię prawdziwej osoby, która musi wykonywać tę nudną, ale bardzo ważną pracę. Mamy nadzieję, że któregoś dnia ta aplikacja pomoże w jej pracy. :)',
+    githubLink: 'https://github.com/CodeForPoznan/alinka-electron',
     image: '',
-    license: '',
-    licensePage: '',
+    license: 'MIT',
+    licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
     name: 'Alinka',
-    partner: '',
-    partnerPage: '',
+    partner: 'Poradnia Pscyhologiczno-Pedagogiczna w Grodzisku Wielkopolskim oraz Poradnia Pscyhologiczno-Pedagogiczna we Wrześni ',
+    partnerPage: 'http://www.grodziskwlkp.naszaporadnia.com/aktualnosci, https://ppp-wrzesnia.pl/',
     stack: [
       {
         type: 'Frontend',
-        name: '',
+        name: 'React',
         version: '',
         documentation: ''
       },
       {
         type: 'Backend',
-        name: '',
+        name: 'electron,sequelize,sqlite3',
         version: '',
         documentation: ''
       }
     ],
-    website: ''
+    website: 'https://alinka.io'
   },
   {
     description: '',
     githubLink: '',
     image: '',
-    license: '',
-    licensePage: '',
+    license: 'MIT',
+    licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
     name: 'Bank Empatii',
     partner: '',
     partnerPage: '',
@@ -98,39 +98,39 @@ const projects = [
         documentation: ''
       }
     ],
-    website: ''
+    website: 'http://bankempatii.pl/'
   },
   {
-    description: '',
-    githubLink: '',
+    description: 'Oryginalny StreetMix jest dziełem CodeforAmerica, które w 2013 roku podczas hacknightu postanowiło stworzyć narzędzie, które pomoże miejskim planistom w przedstawianiu koncepcji ich planu na rewitalizacje miejskiej przestrzeni. Najczęstszym rozwiązaniem po jakie sięgają planiści jest wykonanie makiet z papieru, które zwizualizują krajobraz.  Tworząc internetową wersję tego działania, planiści mogą dotrzeć do szerszego grona odbiorców niż podczas samych spotkań, a także pozwolić członkom społeczności na dzielenie się i remiksowanie swoich dzieł. Jest to jeden z pierwszych naszych projektów, które mieliśmy przyjemność przetłumaczyć tak, aby można było wykorzystać te rozwiązani na polskim gruncie.',
+    githubLink: 'https://github.com/CodeForPoznan/streetmix',
     image: '',
-    license: '',
-    licensePage: '',
+    license: 'BSD 3-Clause',
+    licensePage: 'https://en.wikipedia.org/wiki/BSD_licenses',
     name: 'StreetMix',
     partner: '',
     partnerPage: '',
     stack: [
       {
         type: 'Frontend',
-        name: '',
+        name: 'JavaScript',
         version: '',
         documentation: ''
       },
       {
         type: 'Backend',
-        name: '',
+        name: 'JavaScript',
         version: '',
         documentation: ''
       }
     ],
-    website: ''
+    website: 'https://streetmix.net'
   },
   {
     description: '',
     githubLink: '',
     image: '',
-    license: '',
-    licensePage: '',
+    license: 'MIT',
+    licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
     name: 'Wysadź ulicę',
     partner: '',
     partnerPage: '',
@@ -148,32 +148,32 @@ const projects = [
         documentation: ''
       }
     ],
-    website: ''
+    website: 'http://wysadzulice.pl'
   },
   {
-    description: '',
-    githubLink: '',
+    description: 'Jest to trzecia wersja strony internetowej naszej organizacji. To pokazuje, że jako stowarzyszenie ciągle się rozwijamy i potrzebujemy nowych narzędzi i funkcjonalności do codziennej pracy. Aktualna forma strony jest wynikiem ponad 3 lat działalności jako Code for Poznań.',
+    githubLink: 'https://github.com/CodeForPoznan/codeforpoznan.pl_v3',
     image: '',
-    license: '',
-    licensePage: '',
+    license: 'MIT',
+    licensePage: 'https://pl.wikipedia.org/wiki/Licencja_MIT',
     name: 'Strona społeczności',
     partner: '',
     partnerPage: '',
     stack: [
       {
         type: 'Frontend',
-        name: '',
+        name: 'JavaScript',
         version: '',
         documentation: ''
       },
       {
         type: 'Backend',
-        name: '',
+        name: 'Python',
         version: '',
         documentation: ''
       }
     ],
-    website: ''
+    website: 'https://dev.codeforpoznan.pl/'
   }
 ];
 
