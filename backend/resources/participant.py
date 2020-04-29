7752256ea68b30e097d8708cabb17bfe25c86dcd
w1stler <wojciech.patelka@gmail.com>
2020-04-29 18:58:37 +0200

    [153] CR improvements

--- backend/resources/participant.py
+++ backend/resources/participant.py
@@ -29,16 +29,13 @@ class ParticipantsList(Resource):
         except ValidationError as err:
             return (err.messages), HTTPStatus.BAD_REQUEST
 
-        if Participant.query.filter(Participant.email == json_data["email"]).first():
+        if Participant.query.filter_by(email=json_data["email"]).first():
             return (
                 {"message": "User with this email already exists."},
                 HTTPStatus.CONFLICT,
             )
 
-        if (
-            Participant.query.filter(Participant.github == json_data["github"]).first()
-            is not None
-        ):
+        if Participant.query.filter_by(github=json_data["github"]).first():
             return (
                 {"message": "User with this Github login already exists."},
                 HTTPStatus.CONFLICT,
