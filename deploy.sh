e5df6374673924528d6361a84eed181332dc4cf6
Artur Tamborski <tamborskiartur@gmail.com>
2020-05-19 22:33:33 +0200

    [233] Add deployment step to travis, include deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# build and push frontend
+pushd frontend
+    yarn run build
+    aws s3 sync --quiet dist s3://codeforpoznan-public/dev_codeforpoznan.pl_v3
+    aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
+popd
+
+# build and push backend
+pip install --quiet -r backend/requirements.txt --target packages
+
+pushd packages
+    rm -rf *-info
+    rm -rf psycopg2 # patch psycopg2
+    svn checkout https://github.com/jkehler/awslambda-psycopg2/trunk/psycopg2-3.7
+    mv psycopg2-3.7 psycopg2
+    cp psycopg2/_psycopg*.so psycopg2/_psycopg.so
+    zip -qgr9 ../lambda.zip .
+popd
+
+pushd backend
+    zip -qgr9 ../lambda.zip .
+    aws s3 cp ../lambda.zip s3://codeforpoznan-lambdas/dev_codeforpoznan.pl_v3.zip
+popd
+
+# refresh lambdas
+aws lambda update-function-code                           \
+  --function-name dev_codeforpoznan.pl_v3_serverless_api  \
+  --s3-bucket     codeforpoznan-lambdas                   \
+  --s3-key        dev_codeforpoznan.pl_v3.zip             \
+  --region        eu-west-1                               \
+| jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)' \
+
+aws lambda update-function-code                           \
+  --function-name dev_codeforpoznan.pl_v3_migration       \
+  --s3-bucket     codeforpoznan-lambdas                   \
+  --s3-key        dev_codeforpoznan.pl_v3.zip             \
+  --region        eu-west-1                               \
+| jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)' \
+
+# run migrations
+aws lambda invoke                                         \
+  --function-name dev_codeforpoznan.pl_v3_migration       \
+  --region        eu-west-1                               \
+  response.json                                           \
+> request.json                                            \
+
+# show migration output
+jq -s add ./*.json | jq -re '
+  if .FunctionError then
+    .FunctionError, .errorMessage, false
+  else
+    .stdout, .stderr
+  end'
+
+exit $?
