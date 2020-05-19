e86180f492a366850be5d0bb4debba3b0cbb2fb0
Artur Tamborski <tamborskiartur@gmail.com>
2020-05-19 22:34:25 +0200

    [233] Implement basic lambda handlers

---
+++ backend/handlers.py
@@ -0,0 +1,29 @@
+import os
+from io import StringIO
+from contextlib import redirect_stdout, redirect_stderr
+
+from flask_migrate import migrate
+from serverless_wsgi import handle_request
+
+from backend.app import create_app
+
+
+def api(event, context):
+    event["headers"]["Host"] = os.environ["BASE_URL"]
+    print("REQUEST: ", event)
+    return handle_request(create_app(), event, context)
+
+
+def migration(event, context):
+    stdout = StringIO()
+    stderr = StringIO()
+
+    with redirect_stderr(stderr):
+        with redirect_stdout(stdout):
+            migrate()
+
+    return {
+        "stdout": stdout.getvalue(),
+        "stderr": stderr.getvalue(),
+    }
+
