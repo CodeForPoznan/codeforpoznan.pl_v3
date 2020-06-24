b261289d7e9c7df98f308ef85995a65a30dd3cfa
Artur Tamborski <tamborskiartur@gmail.com>
2020-06-24 16:12:17 +0200

    [233] Add missing /api prefix in few auth.js

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # build and push frontend
 pushd frontend
     yarn run build
-    aws s3 sync --quiet dist s3://codeforpoznan-public/dev_codeforpoznan.pl_v3
+    aws s3 sync dist s3://codeforpoznan-public/codeforpoznan.pl_v3
     aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
 popd
 
