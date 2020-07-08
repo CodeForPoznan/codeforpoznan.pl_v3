dfa1e6982bebef77833b7cabfba5fcc3fab8bd07
OtisRed <durczak.krzysztof@gmail.com>
2020-07-09 00:30:13 +0200

    validations - part 1

--- frontend/src/components/Participants.vue
+++ frontend/src/components/Participants.vue
@@ -1,35 +1,57 @@
 <template>
   <v-container>
     <v-form>
-      <v-text-field v-model="form.first_name" label="First name" />
-      <v-text-field v-model="form.last_name" label="Last name" />
       <v-text-field
-        v-model="form.email"
-        @blur="validateEmail($event)"
-        error-messages="kk"
+        type="text"
         label="E-mail"
+        v-model.trim="form.email"
+        @blur="$v.form.email.$touch()"
+        :error-messages="emailErrors"
+        required
       />
-      <v-text-field v-model="form.phone" label="Phone No." />
-      <v-text-field v-model="form.github" label="Github nick" />
+      <v-text-field
+        label="Github link"
+        v-model="form.githubLink"
+        @blur="$v.form.githubLink.$touch()"
+        :error-messages="githubErrors"
+        required
+      />
+      <v-text-field
+        v-model="form.firstName"
+        label="First name"
+        @blur="$v.form.firstName.$touch()"
+        :error-messages="githubErrors"
+        required
+      />
+      <v-text-field v-model="form.lastName" label="Last name" />
       <v-text-field v-model="form.slack" label="Slack nick" />
-      <v-btn @click="onSubmit">Add new participant</v-btn>
+      <v-text-field v-model="form.phone" label="Phone No." />
+      <v-btn type="submit" :disabled="$v.$invalid" @click="onSubmit"
+        >Add new participant</v-btn
+      >
     </v-form>
   </v-container>
 </template>
 
 <script>
 import { mapState } from 'vuex';
-import { minLength, maxLength, required } from 'vuelidate/lib/validators';
+import {
+  required,
+  minLength,
+  maxLength,
+  email,
+  url
+} from 'vuelidate/lib/validators';
 export default {
   data() {
     return {
       form: {
-        first_name: '',
-        last_name: '',
         email: '',
-        github: '',
-        phone: '',
-        slack: ''
+        githubLink: '',
+        firstName: '',
+        lastName: '',
+        slack: '',
+        phone: ''
       }
     };
   },
@@ -38,10 +60,28 @@ export default {
   },
   validations: {
     form: {
-      first_name: {
+      email: {
+        required,
+        email,
+        emailExists(value) {
+          return !Object.values(this.allParticipants).find(
+            user => user.email === value
+          );
+        }
+      },
+      githubLink: {
+        required,
+        url,
+        githubExists(value) {
+          return !Object.values(this.allParticipants).find(
+            user => user.github === value
+          );
+        }
+      },
+      firstName: {
         required,
         minLength: minLength(3),
-        maxLength: maxLength(100)
+        maxLength: maxLength(25)
       }
     }
   },
@@ -50,21 +90,42 @@ export default {
       const newParticipantData = this.form;
 
       this.$store.dispatch('participant/createParticipant', newParticipantData);
-    },
-    validateEmail(event) {
-      const formEmail = event.target.value;
-      const userExist = !!Object.values(this.allParticipants).find(
-        user => user.email === formEmail
-      );
-
-      return userExist;
-      // console.log('userExist', userExist);
     }
   },
   computed: {
     ...mapState('participant', {
       allParticipants: 'allParticipants'
-    })
+    }),
+    emailErrors() {
+      const errors = [];
+
+      if (!this.$v.form.email.$dirty) return errors;
+      !this.$v.form.email.required && errors.push('This field is required');
+      !this.$v.form.email.email && errors.push('Type a correct e-mail address');
+      !this.$v.form.email.emailExists && errors.push('User already exists');
+      return errors;
+    },
+    githubErrors() {
+      const errors = [];
+
+      if (!this.$v.form.githubLink.$dirty) return errors;
+      !this.$v.form.githubLink.required &&
+        errors.push('This field is required');
+      !this.$v.form.githubLink.url &&
+        errors.push('This field requires URL link to a github account');
+      !this.$v.form.githubLink.githubExists &&
+        errors.push('User already exists');
+      return errors;
+    },
+    firstNameErrors() {
+      const errors = [];
+
+      if (!this.$v.form.firstName.$dirty) return errors;
+      !this.$v.form.firstName.required && errors.push('This field is required');
+      !this.$v.form.firstName.minLength && errors.push('Name is too short');
+      !this.$v.form.firstName.maxLength && errors.push('Name is too long');
+      return errors;
+    }
   }
 };
 </script>
