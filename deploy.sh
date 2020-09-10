6d62714f89742fd6afec99cf3b74cd29c0b80452
Artur Tamborski <tamborskiartur@gmail.com>
2020-09-10 17:24:16 +0200

    [233] Add lambda handler and deployment steps

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,47 @@
 #!/usr/bin/env bash
 
 # build and push frontend
-pushd frontend
-    yarn run build
-    aws s3 sync dist s3://codeforpoznan-public/codeforpoznan.pl_v3
-    aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
-popd
+(cd frontend && yarn run build && cp -r dist ../public)
+aws s3 sync --delete public s3://codeforpoznan-public/codeforpoznan.pl_v3
+aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
 
-# build and push backend
-pip install --quiet -r backend/requirements.txt --target packages
+echo "bundle application"
+pip install -r backend/requirements.txt --target packages
+(cd packages && zip -qgr9 ../lambda.zip .)
+zip -qgr9 ../lambda.zip backend/
+
+echo "upload lambdas"
+aws s3 cp lambda.zip s3://codeforpoznan-lambdas/dev_codeforpoznan_pl_v3_serverless_api.zip
+aws s3 cp lambda.zip s3://codeforpoznan-lambdas/dev_codeforpoznan_pl_v3_migration.zip
+
+echo "refresh lambdas"
+aws lambda update-function-code                              \
+  --function-name dev_codeforpoznan_pl_v3_serverless_api     \
+  --s3-bucket     codeforpoznan-lambdas                      \
+  --s3-key        dev_codeforpoznan_pl_v3_serverless_api.zip \
+  --region        eu-west-1                                  \
+| jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)'    \
+
+aws lambda update-function-code                              \
+  --function-name dev_codeforpoznan_pl_v3_migration          \
+  --s3-bucket     codeforpoznan-lambdas                      \
+  --s3-key        dev_codeforpoznan_pl_v3_migration.zip      \
+  --region        eu-west-1                                  \
+| jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)'    \
+
+echo "run migrations"
+aws lambda invoke                                            \
+  --function-name dev_codeforpoznan_pl_v3_migration          \
+  --region        eu-west-1                                  \
+  response.json                                              \
+> request.json                                               \
+
+echo "show migration output"
+jq -s add ./*.json | jq -re '
+  if .FunctionError then
+    .FunctionError, .errorMessage, false
+  else
+    .stdout, .stderr
+  end'
+
+exit $?
