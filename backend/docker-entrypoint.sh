13d8a5d48028b23bda3a4db38ade3b3033e7d5db
Artur Tamborski <tamborskiartur@gmail.com>
2020-09-20 21:34:41 +0200

    [233] Add lambda handler and deployment steps (#306)
    * [233] Add lambda handler and deployment steps
    
    * [233] Fix DB port on lambda, bucket key for statics and migrations
    
    * [233] Fix linter error

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export FLASK_APP=app.py
-export FLASK_ENV=development
-wait-for-it ${DB_HOST}:5432
+wait-for-it "${DB_HOST}:${DB_PORT:-5432}"
 flask db migrate
 flask db upgrade
 flask run --host=0.0.0.0
