4590b080b3a3f52288c3e577351998c4bfdaacff
w1stler <wojciech.patelka@gmail.com>
2021-01-27 20:32:09 +0100

    [4] Cleanup

--- backend/commands/import_attendance_list.py
+++ backend/commands/import_attendance_list.py
@@ -8,14 +8,13 @@ from backend.extensions import db
 
 @click.command()
 @with_appcontext
-def import_attendance_list():
+def import_attendance_list(filepath_to_csv):
     """Import attendance list."""
     rows = []
     list_of_participants = []
-    users_dict = {}
 
     # open file from exported csv
-    with open("./temp/list.csv") as csv_file:
+    with open(filepath_to_csv) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         for row in csv_reader:
             rows.append(row)
@@ -28,9 +27,14 @@ def import_attendance_list():
     rows = rows[1:]
     # prepare users' data
     for item in rows:
-        _id, github, name, phone, email, *values = item
+        users_dict = {}
+        _id, github, name, phone, email, *_ = item
+
         first_name = name.split()[0]
-        last_name = name.split()[1]
+        try:
+            last_name = name.split()[1]
+        except:
+            last_name = "Unkown"
 
         users_dict["id"] = _id
         users_dict["github"] = github
@@ -38,9 +42,7 @@ def import_attendance_list():
         users_dict["last_name"] = last_name
         users_dict["email"] = email
         users_dict["phone"] = phone
-
         list_of_participants.append(users_dict)
-        users_dict = {}
 
     # create hacknights
     for date in dates:
@@ -57,24 +59,17 @@ def import_attendance_list():
             participant["email"] = f"unkown_email_{idx}"
         if participant["github"] == "":
             participant["github"] = f"unkown_github_{idx}"
-        new_participant = Participant(
-            id=participant["id"],
-            github=participant["github"],
-            first_name=participant["first_name"],
-            last_name=participant["last_name"],
-            email=participant["email"],
-            phone=participant["phone"],
-        )
+        new_participant = Participant(**participant)
         db.session.add(new_participant)
     db.session.commit()
     click.echo(f"Imported {len(list_of_participants)} participants")
 
     # assign attendance list
-    for idx, row in enumerate(rows, 1):
-        for index, element in enumerate(row[5:], 1):
-            participant = Participant.query.get(idx)
+    for participant_idx, row in enumerate(rows, 1):
+        for hacknight_index, element in enumerate(row[5:], 1):
+            participant = Participant.query.get(participant_idx)
             if element == "1":
-                hacknight = Hacknight.query.get(index)
+                hacknight = Hacknight.query.get(hacknight_index)
                 hacknight.participants.append(participant)
                 db.session.commit()
                 click.echo(f"Added {participant.github} to hacknight {hacknight.date}")
