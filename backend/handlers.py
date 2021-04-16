06d42d2cf54bf8ec3da25a9519b7f5c8de28779d
Artur Tamborski <tamborskiartur@gmail.com>
2021-04-16 10:31:58 +0200

    Simplify migration() handler, use new wrap_io()

--- backend/handlers.py
+++ backend/handlers.py
@@ -1,12 +1,10 @@
 import os
-import sys
-from io import StringIO
-from contextlib import redirect_stdout, redirect_stderr
 
 from flask_migrate import init, stamp, migrate, upgrade
 from serverless_wsgi import handle_request
 
 from backend.app import create_app
+from backend.helpers import wrap_io
 
 
 def api(event, context):
@@ -16,47 +14,19 @@ def api(event, context):
 
 
 def migration(event, context):
-    out, err = wrap_io(migrate)
+    with create_app().app_context():
+        # db commands can with exit(1) - we _have_ to catch that
+        with wrap_io(catch=BaseException) as (out, err):
 
-    if "Please use the 'init' command" in err:
-        wrap_io(init)
-        out, err = wrap_io(migrate)
+            migrate()
 
-    if "Target database is not up to date" in err:
-        wrap_io(stamp)
-        wrap_io(migrate)
-        out, err = wrap_io(upgrade)
+            if "Please use the 'init' command" in err():
+                init()
+                migrate()
 
-    return {"stdout": out, "stderr": err}
+            if "Target database is not up to date" in err():
+                stamp()
+                migrate()
+                upgrade()
 
-
-# TODO: rewrite as contextmanager?
-def wrap_io(fn: callable) -> (str, str):
-    out = StringIO()
-    err = StringIO()
-
-    # noinspection PyBroadException
-    try:
-        with redirect_stderr(err):
-            with redirect_stdout(out):
-                with create_app().app_context():
-                    print(f"Running {fn.__name__}")
-                    fn()
-
-    # catch-all case for potential SystemExit(1)
-    except BaseException:
-        pass
-
-    out = out.getvalue()
-    err = err.getvalue()
-
-    # print captured outputs
-    sys.stdout.write(f"stdout: {out}")
-    sys.stderr.write(f"stderr: {err}")
-
-    # a cheap state caching trick, but does the job
-    wrap_io._out = getattr(wrap_io, "_out", "") + out
-    wrap_io._err = getattr(wrap_io, "_err", "") + err
-    out, err = wrap_io._out, wrap_io._err
-
-    return out, err
+    return {"stdout": out(), "stderr": err()}
