82bd80d9f413bd22739343a0effd44fb144b5a1c
Artur Tamborski <tamborskiartur@gmail.com>
2021-04-20 01:08:53 +0200

    Fix issue with current_app() scope, add second message

--- backend/resources/contact.py
+++ backend/resources/contact.py
@@ -3,8 +3,7 @@ from textwrap import dedent
 
 from http import HTTPStatus
 
-from flask import request
-from flask import current_app
+from flask import request, current_app
 from flask_restful import Resource
 
 from marshmallow import ValidationError
@@ -14,12 +13,32 @@ from backend.extensions import mail
 from backend.helpers import wrap_io
 
 
-MAIL_RECIPIENT = current_app.config["MAIL_RECIPIENT"]
-MAIL_WEB_SERVER = current_app.config["MAIL_WEB_SERVER"]
-BASE_URL = current_app.config["BASE_URL"]
-
-
 class SendMessage(Resource):
+
+    internal_message_subject = """
+    """
+
+    internal_message_body = """
+    """
+
+    external_message_subject = """
+        Email z {base_url} od {name}
+    """
+
+    external_message_body = """
+        Nowa wiadomość od {name}
+        numer telefonu: {phone}
+        Treść:
+        {content}
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.base_url = current_app.config["BASE_URL"]
+        self.mail_recipient = current_app.config["MAIL_RECIPIENT"]
+        self.mail_web_server = current_app.config["MAIL_WEB_SERVER"]
+
     def post(self):
         try:
             message_schema = MessageSchema()
@@ -27,47 +46,55 @@ class SendMessage(Resource):
         except ValidationError as err:
             return {"message": err.messages}, HTTPStatus.BAD_REQUEST
 
-        name, email = data["name"], data["email"]
-        phone, content = data["phone"], data["content"]
+        sender_email = data["email"]
+        context = {
+            **data,
+            'base_url': self.base_url,
+            'mail_recipient': self.mail_recipient,
+        }
 
-        subject = f"""
-            Email z {BASE_URL} od {name}
-        """
+        try:
+            # outgoing mail from someone (via form) to us, send internal message
+            self.send_message(
+                subject=self.internal_message_subject,
+                body=self.internal_message_body,
+                context=context,
+                sender=sender_email,
+                reply_to=sender_email,
+                recipients=[self.mail_recipient],
+            )
 
-        body = f"""
-            Nowa wiadomość od {name}
-            numer telefonu: {phone}
-            Treść:
-            {content}
-        """
+            # outgoing mail from us to someone, send pretty external message
+            self.send_message(
+                subject=self.external_message_subject,
+                body=self.external_message_body,
+                context=context,
+                sender=self.mail_recipient,
+                reply_to=self.mail_recipient,
+                recipients=[sender_email],
+            )
+        except:
+            return {"message": "Contact message successfully sent"}, HTTPStatus.OK
 
-        status_code = self._send_message(
-            sender=email,
-            reply_to=email,
-            recipients=[MAIL_RECIPIENT],
-            subject=dedent(subject).strip(),
-            body=dedent(body).strip(),
-        )
+        return {"message": "Failed to send contact message"}, HTTPStatus.SERVICE_UNAVAILABLE
 
-        if status_code == HTTPStatus.OK:
-            return {"message": "Contact message successfully sent"}, status_code
-        else:
-            return {"message": "Failed to send contact message"}, status_code
+    def send_message(self, *, subject: str, body: str, context: dict, **kwargs):
+        subject = dedent(subject).strip().format(**context)
+        body = dedent(body).strip().format(**context)
 
-    @staticmethod
-    def _send_message(**kwargs):
         with wrap_io() as (out, err):
-            mail.send_message(**kwargs)
+            mail.send_message(subject=subject, body=body, **kwargs)
 
         # known mail testing domain, find message link in output
-        if MAIL_WEB_SERVER:
+        if self.mail_web_server:
             if "MSGID=" in err():
                 msgid = err().split("MSGID=")[1].split("]")[0]
                 link = f"https://ethereal.email/message/{msgid}"
                 logging.info(f"Mail sent successfully: {link}")
-                return HTTPStatus.OK
             else:
                 logging.error("Failed to send mail, SMTP stderr:\n", err())
+                # TODO: raise 503
+                # TODO: fix logging
                 return HTTPStatus.SERVICE_UNAVAILABLE
 
         # real smtp server
