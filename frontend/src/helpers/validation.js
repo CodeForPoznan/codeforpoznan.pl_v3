4fb4acd35b2c6fe7c7ae97e433eef6dc72c72a5e
Marcelina Kucięba <marcelinakucieba@gmail.com>
2021-05-05 11:10:08 +0200

    Change chars order in slackNickValidator

--- frontend/src/helpers/validation.js
+++ frontend/src/helpers/validation.js
@@ -2,7 +2,7 @@ import { helpers } from 'vuelidate/lib/validators';
 
 export const slackNickValidator = helpers.regex(
   'slackNickValidator',
-  /^[0-9A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ,.';\-_/()[\]{}]*$/
+  /^[0-9A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ,.';\-_/()[\]{}]*$/
 );
 
 export const gitHubUsernameValidator = helpers.regex(
