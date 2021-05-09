76be017c4280e3338b16a2586c57383b37e2ff88
Artur Tamborski <tamborskiartur@gmail.com>
2021-05-09 11:55:53 +0200

    Use deployment env variables

--- deploy.sh
+++ deploy.sh
@@ -2,61 +2,73 @@
 set -Eeuo pipefail
 
 
-resource="${1:-dev_codeforpoznan_pl_v3}"
+case "${1:-}" in
+  staging)
+    echo "deploying to staging"
+    export RESOURCE="${STAGING_RESOURCE}"
+    export AWS_ACCESS_KEY_ID="${STAGING_ACCESS_KEY_ID}"
+    export AWS_SECRET_ACCESS_KEY="${STAGING_SECRET_ACCESS_KEY}"
+  ;;
 
+  production)
+    echo "deploying to production"
+    export RESOURCE="${PRODUCTION_RESOURCE}"
+    export AWS_ACCESS_KEY_ID="${PRODUCTION_ACCESS_KEY_ID}"
+    export AWS_SECRET_ACCESS_KEY="${PRODUCTION_SECRET_ACCESS_KEY}"
+  ;;
 
-echo "deploying to '${resource}'"
-[[ "${resource}" ]] || exit 1
+  *)
+    echo "invalid environment '${1}', exiting..."
+    exit 1
+  ;;
+esac
 
 
 echo "build and push frontend"
 (cd frontend && yarn run build && cp -r dist ../public)
-aws s3 sync --delete public "s3://codeforpoznan-public/${resource}"
+aws s3 sync --delete public "s3://codeforpoznan-public/${RESOURCE}"
 
 
 echo "refresh CDN"
 dist_id=$(
   aws cloudfront list-distributions \
-    | jq --arg PATH "/$resource" -r '
+    | jq --arg PATH "/${RESOURCE}" -r '
       .DistributionList.Items[]
       | {Id, p: .Origins.Items[].OriginPath}
       | select(.p == $PATH).Id'
 )
-aws cloudfront create-invalidation --paths "/*" --distribution-id "$dist_id"
+aws cloudfront create-invalidation --paths "/*" --distribution-id "${dist_id}"
 
 
 echo "bundle application"
-(cd backend && pipenv run pip install -r <(pipenv lock -r) --target ../packages)
+(cd backend  && pipenv run pip install -r <(pipenv lock -r) --target ../packages)
 (cd packages && zip -qgr9 ../lambda.zip .)
-ln -s backend/migrations migrations
+ln  --symbolic backend/migrations migrations
 zip --symlinks -qgr9 lambda.zip backend/ migrations/
 
 
 echo "upload lambdas"
-aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${resource}_serverless_api.zip"
-aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${resource}_migration.zip"
+aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${RESOURCE}_serverless_api.zip"
+aws s3 cp lambda.zip "s3://codeforpoznan-lambdas/${RESOURCE}_migration.zip"
 
 
 echo "refresh lambdas"
 aws lambda update-function-code                           \
-  --function-name "${resource}_serverless_api"            \
   --s3-bucket     "codeforpoznan-lambdas"                 \
-  --s3-key        "${resource}_serverless_api.zip"        \
-  --region        "eu-west-1"                             \
+  --s3-key        "${RESOURCE}_serverless_api.zip"        \
+  --function-name "${RESOURCE}_serverless_api"            \
 | jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)' \
 
 aws lambda update-function-code                           \
-  --function-name "${resource}_migration"                 \
   --s3-bucket     "codeforpoznan-lambdas"                 \
-  --s3-key        "${resource}_migration.zip"             \
-  --region        "eu-west-1"                             \
+  --s3-key        "${RESOURCE}_migration.zip"             \
+  --function-name "${RESOURCE}_migration"                 \
 | jq 'del(.Environment, .VpcConfig, .Role, .FunctionArn)' \
 
 
 echo "run migrations"
 aws lambda invoke                         \
-  --function-name "${resource}_migration" \
-  --region        "eu-west-1"             \
+  --function-name "${RESOURCE}_migration" \
   response.json                           \
 > request.json                            \
 
