ccc94f35af7db8be2405f1f53bc687874a34688e
Marcelina Kucięba <marcelinakucieba@gmail.com>
2021-08-18 20:50:47 +0200

    Add date range picker for participants chart

--- frontend/src/components/ParticipantsChart.vue
+++ frontend/src/components/ParticipantsChart.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="participants-chart-container">
-    <h2 class="d-flex justify-center">Participants on hacknights</h2>
+    <h2>Participants on hacknights</h2>
+    <div class="range-container">
+      <div class="range">
+        <div class="start-date">
+          <label for="start-date">Start date:</label>
+          <input
+            type="date"
+            id="start-date"
+            name="start-date"
+            v-model="startDate"
+            class="range-input"
+          />
+        </div>
+        <div class="end-date">
+          <label for="end-date">End date:</label>
+          <input
+            type="date"
+            id="end-date"
+            name="end-date"
+            v-model="endDate"
+            class="range-input"
+          />
+        </div>
+      </div>
+    </div>
     <apexchart
       width="500"
       type="line"
@@ -12,12 +36,32 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import { getDateString } from '../helpers/date';
 
 export default {
+  data() {
+    return {
+      todayDate: new Date(),
+      startDate: null,
+      endDate: null
+    };
+  },
+  mounted() {
+    const todayDateCopy = new Date(this.todayDate);
+
+    todayDateCopy.setMonth(todayDateCopy.getMonth() - 12);
+
+    this.startDate = getDateString(todayDateCopy);
+    this.endDate = getDateString(this.todayDate);
+  },
   computed: {
     ...mapGetters('hacknight', ['getHacknights']),
     hacknightsToDisplay() {
-      return this.getHacknights.slice(0, 6).reverse();
+      return this.getHacknights
+        .filter(({ date }) => {
+          return date > this.startDate && date < this.endDate;
+        })
+        .reverse();
     },
     chartCategories() {
       return this.hacknightsToDisplay.map(hacknight => hacknight.date);
@@ -54,5 +98,33 @@ export default {
 <style>
 .participants-chart-container {
   margin: 60px;
+  text-align: center;
+}
+.range-container {
+  display: flex;
+  margin: 25px 0;
+  flex-direction: column;
+  align-items: center;
+}
+.range {
+  display: flex;
+  margin-top: 10px;
+  align-items: baseline;
+  color: #6e8192;
+}
+.range-input {
+  border: 1px solid black;
+  color: #6e8192;
+  border-radius: 5px;
+  margin-right: 5px;
+  padding: 4px 10px;
+}
+.start-date,
+.end-date {
+  display: flex;
+  flex-direction: column;
+}
+.end-date {
+  margin-left: 20px;
 }
 </style>
